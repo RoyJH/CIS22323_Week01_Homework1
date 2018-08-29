55b717312a61fadbe8bf8b4c6dad444370fa09a7
RoyJH <royjh@okstate.edu>
2018-08-29 11:25:35 -0500

    Initial commit

---
+++ Card.java
@@ -0,0 +1,12 @@
+public class Card{
+	public static void main(String[] args){
+		System.out.println("<========--------OSU_OKC_CIS:2323--------========>");
+		System.out.println("| Roy J. Harris III 'Trey'			 |");
+		System.out.println("|			        royjh@okstate.edu|");
+		System.out.println("| 1006 W Ridgecrest Way,  	github.com/royjh |");
+		System.out.println("| Mustang, OK 73064	      Phone:405.613.2591 |");
+		System.out.println("|						 |");
+		System.out.println("<========--------Java_Programming--------========>");
+
+	}
+}
